7f8d6802c44eead6b12e7c0c62f4bd96c3028d38
CJ <cj@wolfssl.com>
2014-05-16 12:00:46 -0600

    final working basic tcp client

--- client-tcp.c
+++ client-tcp.c
@@ -1,33 +1,54 @@
-#include    <stdio.h>               /* useful libraries to be included */
+#include    <stdio.h>               /* must include these libraries */
 #include    <stdlib.h>                  
-#include    <sys/socket.h>
-#include    <netinet/in.h>
 #include    <string.h>
-#include    <unistd.h>
 #include    <errno.h>
 #include    <arpa/inet.h>
-#include    <signal.h>
 
-/* Define default port number */
-#define SERV_PORT   1337
+#define SERV_PORT   11111   /* Define default port number */
+#define MAXDATASIZE 4096    /* Maximum acceptable amount of data */
 
-int Socket(int, int, int);
-void Client(FILE *filePtr, int socket)
-{
-    
-}
-/* number of command line arguments, the arguments themselves */
+/* Command line argumentCount and argumentValues */
 int main(int argc, char** argv) 
 {
-    int     socket;
-    struct  sockAddr_in servAddr;
+    int     sockfd;                             /* Socket File Discriptor */
+    struct  sockaddr_in servAddr;               /* Struct forServer Address */
+    char    send[] = "Initiating Contact...";   /* Data sent to server */
+    char*   msg = send;                         /* Pointer for data */
+    char    recieve[MAXDATASIZE];               /* Recived data from server*/
 
     /* if the number of arguments is not two, error */
     if(argc != 2) {
         printf("usage: ./client-tcp  <IP address>\n");
         exit(EXIT_FAILURE);
     }
-    socket = Socket(AF_INET, SOCK_STREAM, 0);
+    /* Internet Address Family, Stream based tcp, default protocol */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) {                        /* If the socket fails */
+        printf("Failed to create socket\n");
+        exit(EXIT_FAILURE);
+    }
 
+    bzero(&servAddr, sizeof(servAddr));     /* Clears memory block for use */
+    servAddr.sin_family = AF_INET;          /* Sets AddressFamily to internet*/
+    servAddr.sin_port = htons(SERV_PORT);   /* Sets port to defined port */
+    /* Looks for the Server at the entered address (IP in the command line) */
+    inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
+
+    /* If socket fails to connect to the server*/
+    if(connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+        printf("Connect error: %i\n", errno);
+        exit(EXIT_FAILURE);
+    }
+    /* If the message is not able to send */
+    if(write(sockfd, msg, strlen(msg)) != strlen(msg)) {
+        printf("Written error\n");
+        exit(EXIT_FAILURE);
+    }
+    /* If the server fails to return data */
+    if(read(sockfd, recieve, MAXDATASIZE) == 0) {
+        printf("Server terminated prematurely\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("%s\n",recieve); /* Print out the string recieved from the server */
     return 0;
 } 
