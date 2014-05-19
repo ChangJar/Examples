66bf151e3bfbc0bec01365e5d047f96c6bef6f0e
CJ <cj@wolfssl.com>
2014-05-19 16:34:35 -0600

    mostly finished source code and make files

--- Secured/client-tcp.c
+++ Secured/client-tcp.c
@@ -23,14 +23,15 @@
 #include    <string.h>
 #include    <errno.h>
 #include    <arpa/inet.h>
+#include    <cyassl/ssl.h>          /* cyaSSL security library */
 
 #define MAXDATASIZE  4096   /* maximum acceptable amount of data */
 #define SERV_PORT    11111  /* define default port number */
 
-/*
- *  clients initial contact with server. Socket to connect to: sock
+/* 
+ * clients initial contact with server. (socket to connect, security layer)
  */
-void ClientGreet(int sock)
+void ClientGreet(int sock, CYASSL* ssl)
 {
     /* data to send to the server, data recieved from the server */
     char send[MAXDATASIZE], recieve[MAXDATASIZE];
@@ -38,28 +39,59 @@ void ClientGreet(int sock)
     printf("Message for server:\t");
     fgets(send, MAXDATASIZE, stdin);
 
-    if (write(sock, send, strlen(send)) != strlen(send)) {
+    if (CyaSSL_write(ssl, send, strlen(send)) != strlen(send)) {
         /* the message is not able to send, or error trying */
-        printf("Write error: errno: %i\n", errno);
+        printf("Writte error: errno: %i\n", errno);
         exit(EXIT_FAILURE);
     }
 
-    if (read(sock, recieve, MAXDATASIZE) == 0) {
-        /* the server fails to send data, or error trying */
+    if (CyaSSL_read(ssl, recieve, MAXDATASIZE) == 0) {
+        /* the server failed to send data, or error trying */
         printf("Read error. errno: %i\n", errno);
         exit(EXIT_FAILURE);
     }
-
     printf("Recieved: \t%s\n", recieve);
 }
-
 /* 
- * command line argumentCount and argumentValues 
+ * applies TLS 1.2 security layer to data being sent.
+ */
+void security(int sock)
+{
+    CyaSSL_Init();      /* initialize CyaSSL (must be done first) */
+    CYASSL_CTX* ctx;
+    CYASSL*     ssl;    /* create CYASSL object */
+
+    /* create and initiLize CYASSL_CTX structure */
+    if ((ctx = CyaSSL_CTX_new(CyaTLSv1_2_client_method())) == NULL) {
+        printf("SSL_CTX_new error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* load CA certificates into CyaSSL_CTX. which will verify the server */
+    if (CyaSSL_CTX_load_verify_locations(ctx, "../certs/ca-cert.pem",0) != 
+            SSL_SUCCESS) {
+        printf("Error loading ./certs/ca-cert.pem. Please check the file.\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((ssl = CyaSSL_new(ctx)) == NULL) {
+        printf("CyaSSL_new error.\n");
+        exit(EXIT_FAILURE);
+    }
+    CyaSSL_set_fd(ssl, sock);
+    ClientGreet(sock, ssl);
+
+    /* frees all data before client termination */
+    CyaSSL_free(ssl);
+    CyaSSL_CTX_free(ctx);
+    CyaSSL_Cleanup();
+}
+/* 
+ * Command line argumentCount and argumentValues 
  */
 int main(int argc, char** argv) 
 {
     int     sockfd;                             /* socket file discriptor */
-    struct  sockaddr_in servAddr;               /* Struct for Server Address */
+    struct  sockaddr_in servAddr;               /* struct for server address */
 
     if (argc != 2) {
         /* if the number of arguments is not two, error */
@@ -76,22 +108,21 @@ int main(int argc, char** argv)
     }
 
     /* clears memory block for use */
-    bzero(&servAddr, sizeof(servAddr));
-
+    bzero(&servAddr, sizeof(servAddr));    
     /* sets addressfamily to internet*/
-    servAddr.sin_family = AF_INET;  
-
+    servAddr.sin_family = AF_INET;         
     /* sets port to defined port */
-    servAddr.sin_port = htons(SERV_PORT);
+    servAddr.sin_port = htons(SERV_PORT);  
 
     /* looks for the server at the entered address (ip in the command line) */
     inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
 
     if (connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+        /* if socket fails to connect to the server*/
         printf("Connect error. errno: %i\n", errno);
         exit(EXIT_FAILURE);
     }
-    ClientGreet(sockfd);
+    security(sockfd);
     return 0;
 }
 
