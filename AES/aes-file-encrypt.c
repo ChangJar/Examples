7b9ab1dfd4957803de7d17e6140c4ff78141c339
CJ <cj@wolfssl.com>
2014-06-04 11:26:52 -0600

    Randomized iv and Camellia implementation

--- AES/aes-file-encrypt.c
+++ AES/aes-file-encrypt.c
@@ -18,21 +18,26 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
  */
 
-#include    <stdio.h>
-#include    <cyassl/ctaocrypt/aes.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <cyassl/options.h>
+#include <cyassl/ctaocrypt/aes.h>
+#include <cyassl/ctaocrypt/random.h>
 
 char choice;                            /* option entered in commandline */
 
 int AesTest(char* fileIn, char* fileOut, byte* key)
 {
-    FILE* inFile =  fopen(fileIn, "r"); /* file used to take message from */
-    FILE* outFile = fopen(fileOut, "w");/* file used to write message to */
+    FILE* inFile =  fopen(fileIn, "ra"); /* file used to take message from */
+    FILE* outFile = fopen(fileOut, "wa");/* file made to write message to */
 
     Aes enc;                            /* AES for encoding */
     Aes dec;                            /* AES for decoding */
+    RNG rng;                            /* random number generator */
 
     /* Initialization vector: used for randomness of encryption */
-    byte iv[] = "onetwothreefour"; 		/* should be random or pseudorandom */
+    byte iv[AES_BLOCK_SIZE];            /* should be random or pseudorandom */
+    int i;                              /* loop counter */
     int ret;                            /* return variable for errors */
     long numBlocks;                     /* number of ASE blocks for encoding */
     int padCounter = 0;                 /* number of padded bytes */
@@ -41,7 +46,7 @@ int AesTest(char* fileIn, char* fileOut, byte* key)
     fseek(inFile, 0, SEEK_END);
     int inputLength = ftell(inFile);       /* length of message */
     fseek(inFile, 0, SEEK_SET);
-    int length;                         /* length of input after padding */
+    int length;                            /* length of input after padding */
     
     length = inputLength;    
     /* pads the length until it evenly matches a block / increases pad number*/
@@ -55,19 +60,20 @@ int AesTest(char* fileIn, char* fileOut, byte* key)
     /* reads from inFile and writes whatever is there to the input array */
     fread(input, 1, length, inFile);
 
-    int i;                              /* loop counter */
     for (i = inputLength; i < length; i++) {
         /* padds the added characters with the number of pads */
-        input[i] = 0%padCounter;
+        input[i] = padCounter;
     } 
     /* finds the number of encoding blocks to be used */
-    numBlocks = length/AES_BLOCK_SIZE;
+    numBlocks = length / AES_BLOCK_SIZE;
 
     byte output[AES_BLOCK_SIZE * numBlocks];/* outFile message[] */
 
     if (choice == 'e') {
         /* if encryption was the chosen option 
-        set encryption key. must have key to decrypt */
+        /* randomly generates iv */
+        RNG_GenerateBlock(&rng, iv, AES_BLOCK_SIZE);
+        /* sets key */
         ret = AesSetKey(&enc, key, AES_BLOCK_SIZE, iv, AES_ENCRYPTION);
         if (ret != 0)
             return -1001;
@@ -77,30 +83,38 @@ int AesTest(char* fileIn, char* fileOut, byte* key)
         if (ret != 0)
             return -1005;
 
+        /* writes iv to outFile */
+        fwrite(iv, 1, AES_BLOCK_SIZE, outFile);
         /* writes output to outFile */
         fwrite(output, 1, length, outFile);
     }
     if (choice == 'd') {
-        /* if decryption was the chosen option
-        sets the key to use, if it matches the encryption key success */
+        /* if decryption was the chosen option */
+        for (i = 0; i < AES_BLOCK_SIZE; i++) {
+            /* finds iv from input message */
+            iv[i] = input[i];
+        }
+        /* sets key */
         ret = AesSetKey(&dec, key, AES_BLOCK_SIZE, iv, AES_DECRYPTION);
         if (ret != 0)
             return -1002;
 
+        /* change length to remove iv block from beyind decrypted */
+        length-=AES_BLOCK_SIZE;
+        for (i = 0; i < length; i++) {
+            /* shifts message over an encryption block: ignores iv on message*/
+            input[i] = input[i + AES_BLOCK_SIZE];
+        }
         /* decrypts the message to output based on input length + padding */
         ret = AesCbcDecrypt(&dec, output, input, length);
         if (ret != 0)
             return -1006;
 
-        int i;                              /* loop counter */
-        /* checks the last block for padding */
-        for (i = AES_BLOCK_SIZE * (numBlocks - 1); i < length; i++) {
-            if(output[i] == output[length-1])
-                inputLength--;
-        }
-
+        /* reduces length based on number of padded elements */
+        inputLength -= output[length-1];
+        
         /* writes output to the outFile based on shortened length */
-        fwrite(output, 1, inputLength, outFile);
+        fwrite(output, 1, inputLength-AES_BLOCK_SIZE, outFile);
     }
     /* closes the opened files */
     fclose(inFile);
@@ -111,18 +125,19 @@ int AesTest(char* fileIn, char* fileOut, byte* key)
  void help()
  {
     printf("\n~~~~~~~~~~~~~~~~~~~~|Help|~~~~~~~~~~~~~~~~~~~~~\n\n");
-        printf("Usage: ./aes-file-encrypt <-option> <file.in> <file.out>"
-                " <key>\n\n");
+        printf("Usage: ./aes-file-encrypt <-option> <file.in> <file.out>\n\n");
         printf("Options\n");
         printf("-d    Decpription\n-e    Encryption\n-h    Help\n");
  }
  int main(int argc, char** argv)
  {
     int option;
+    char* key;
 
-    if (argc != 5) /* if number of arguments is not 5 'help' */
+    if (argc != 4) /* if number of arguments is not 4 'help' */
         help();
     else {
+        key = getpass("Key: ");
         while ((option = getopt(argc, argv, "deh:")) != -1) {
             switch (option) {
                 case 'd': /* if entered decrypt */
@@ -138,7 +153,7 @@ int AesTest(char* fileIn, char* fileOut, byte* key)
                     abort();
             }
         }
-        AesTest(argv[2], argv[3], argv[4]);
+        AesTest(argv[2], argv[3], key);
     }
     return 0;
  }
--- Camellia/camellia-encrypt.c
+++ Camellia/camellia-encrypt.c
@@ -1,4 +1,4 @@
-/* 3des-file-encrypt.c
+/* camellia-encrypt.c
  *
  * Copyright (C) 2006-2014 wolfSSL Inc.
  * This file is part of CyaSSL.
@@ -17,125 +17,115 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
  */
-
 #include <stdio.h>
 #include <unistd.h>
 #include <cyassl/options.h>
-#include <cyassl/ctaocrypt/des3.h>
 #include <cyassl/ctaocrypt/random.h>
+#include <cyassl/ctaocrypt/camellia.h>
 
-#define DES3_BLOCK_SIZE 24               /* size of encryption blocks */
+char choice;
 
-char choice;                             /* option entered in commandline */
-
-int Des3Test(char* fileIn, char* fileOut, byte* key)
+int CamelliaTest(char* fileIn, char* fileOut, byte* key)
 {
-	FILE* inFile =	fopen(fileIn, "ra"); /* file used to take message from */
-	FILE* outFile =	fopen(fileOut, "wa");/* file made to write messge to */
+    FILE* inFile =  fopen(fileIn, "ra");/* file used to take message from */
+    FILE* outFile = fopen(fileOut, "wa");/* file made to wrtie message to */
 
-	Des3 enc;                           /* 3DES for encoding */
-	Des3 dec;                           /* 3DES for decoding */
-    RNG rng;
+    Camellia cam;                       /* camellia for encoding/decoding */
+    RNG rng;                            /* random number generator */
 
     /* Initialization vector: used for randomness of encryption */
-    byte iv[DES3_BLOCK_SIZE];           /* should be random or pseudorandom */
+    byte iv[CAMELLIA_BLOCK_SIZE];       /* should be random or pseudorandom */
     int i;                              /* loop counter */
-	int ret;                            /* return variable for errors */
-    long numBlocks;                     /* number of encryption blocks */
-	int padCounter = 0;                 /* number of padded bytes */
+    int ret;                            /* return variable for errors */
+    long numBlocks;                     /* number of ASE blocks for encoding */
+    int padCounter = 0;                 /* number of padded bytes */
 
-    /* finds the end of inFile to determine length  */
-	fseek(inFile, 0, SEEK_END);
-    int inputLength = ftell(inFile);    /* length of message */
+    /* finds the end of inFile to determine length */
+    fseek(inFile, 0, SEEK_END);
+    int inputLength = ftell(inFile);       /* length of message */
     fseek(inFile, 0, SEEK_SET);
-    int length;                         /* length of input after padding */
-
-    length = inputLength;
+    int length;                            /* length of input after padding */
+    
+    length = inputLength;    
     /* pads the length until it evenly matches a block / increases pad number*/
-    while (length % DES3_BLOCK_SIZE != 0) {
+    while(length % CAMELLIA_BLOCK_SIZE != 0) {
         length++;
         padCounter++;
     }
 
-    byte input[length];                 /* actual message */
+    byte input[length];                   /* actual message */
 
-    /* reads from inFile and wrties whatever is there to the input array */
+    /* reads from inFile and writes whatever is there to the input array */
     fread(input, 1, inputLength, inFile);
     for (i = inputLength; i < length; i++) {
         /* padds the added characters with the number of pads */
         input[i] = padCounter;
-    }
-    /*  finds the number of encoding blocks to be used*/
-    numBlocks = length / DES3_BLOCK_SIZE;
+    } 
+    /* finds the number of encoding blocks to be used */
+    numBlocks = length / CAMELLIA_BLOCK_SIZE;
 
-    byte output[DES3_BLOCK_SIZE * numBlocks];/* outFile message[] */
+    byte output[CAMELLIA_BLOCK_SIZE * numBlocks];/* outFile message[] */
 
-	if (choice == 'e') {
-        /* if encryption was the chosen option
-        set encryption key. must have key to decrypt */
-        RNG_GenerateBlock(&rng, iv, DES3_BLOCK_SIZE);
+    if (choice == 'e') {
+        /* if encryption was the chosen option */
+        /* randomly generates iv */
+        RNG_GenerateBlock(&rng, iv, CAMELLIA_BLOCK_SIZE);
+        /* sets new randomized iv */
+        CamelliaSetIV(&cam, iv);
         /* sets key */
-        ret = Des3_SetKey(&enc, key, iv, DES_ENCRYPTION);
-        if (ret != 0)
-            return -1001;
-
+        CamelliaSetKey(&cam, key, CAMELLIA_BLOCK_SIZE, iv);
         /* encrypts the message to the ouput based on input length + padding */
-		ret = Des3_CbcEncrypt(&enc, output, input, length);
-		if (ret != 0)
-			return -1005;
-
+        CamelliaCbcEncrypt(&cam, output, input, length);
         /* writes iv to outFile */
-        fwrite(iv, 1, DES3_BLOCK_SIZE, outFile);
+        fwrite(iv, 1, CAMELLIA_BLOCK_SIZE, outFile);
         /* writes output to outFile */
-		fwrite(output, 1, length, outFile);
-	}
-	if (choice == 'd') {
+        fwrite(output, 1, length, outFile);
+    }
+    if (choice == 'd') {
         /* if decryption was the chosen option */
-        for (i = 0; i < DES3_BLOCK_SIZE; i++) {
+        for (i = 0; i < CAMELLIA_BLOCK_SIZE; i++) {
             /* finds iv from input message */
             iv[i] = input[i];
         }
+        /* sets iv to found iv */
+        CamelliaSetIV(&cam, iv);
         /* sets key */
-        ret = Des3_SetKey(&dec, key, iv, DES_DECRYPTION);
-        if (ret != 0)
-            return -1002;
+        CamelliaSetKey(&cam, key, CAMELLIA_BLOCK_SIZE, iv);
 
         /* change length to remove iv block from being decrypted */
-        length-=DES3_BLOCK_SIZE;
+        length-=CAMELLIA_BLOCK_SIZE;
         for (i = 0; i < length; i++) {
             /* shifts message over an encryption block: ignores iv on message*/
-            input[i] = input[i + DES3_BLOCK_SIZE];
+            input[i] = input[i + CAMELLIA_BLOCK_SIZE];
         }
-        /* decrypts the message to output based on input length + padding*/
-		ret = Des3_CbcDecrypt(&dec, output, input, length);
-		if (ret != 0)
-			return -1006;
+        /* decrypts the message to output based on input */
+        CamelliaCbcDecrypt(&cam, output, input, length);
 
         /* reduces length based on number of padded elements */
         inputLength -= output[length-1];
 
         /* writes output to the outFile based on shortened length */
-     	fwrite(output, 1, inputLength - DES3_BLOCK_SIZE, outFile);
-	}
+        fwrite(output, 1, inputLength - CAMELLIA_BLOCK_SIZE, outFile);
+    }
     /* closes the opened files */
-	fclose(outFile);
-	fclose(inFile);
+    fclose(inFile);
+    fclose(outFile);
 
-	return 0;
+    return 0;
 }
 void help()
 {
-	printf("\n~~~~~~~~~~~~~~~~~~~~|Help|~~~~~~~~~~~~~~~~~~~~~\n\n");
-    printf("Usage: ./3des-file-encrypt <-option> <file.in> <file.out>\n\n");
+    printf("\n~~~~~~~~~~~~~~~~~~~~|Help|~~~~~~~~~~~~~~~~~~~~~\n\n");
+    printf("Usage: ./camellia-encrypt <-option> <file.in> <file.out>\n\n");
     printf("Options\n");
     printf("-d    Decpription\n-e    Encryption\n-h    Help\n");
 }
 int main(int argc, char** argv)
 {
-	int option;
+    int option;
     char* key;
 
-	if (argc != 4) /* if number of arguments is not 4 'help' */
+    if (argc != 4) /* if number of arguments is not 4 'help' */
         help();
     else {
         key = getpass("Key: ");
@@ -148,13 +138,13 @@ int main(int argc, char** argv)
                     choice = 'e';
                     break;
                 case 'h': /* if entered 'help' */
-                	help();
-                	break;
+                    help();
+                    break;
                 default:
                     abort();
             }
         }
-        Des3Test(argv[2], argv[3], key);
+        CamelliaTest(argv[2], argv[3], key);
     }
-	return 0;
+    return 0;
 }
